e21b91be7021e4d61fac5db8750971dc5305771b
gossi <gossi@users.noreply.github.com>
2023-08-01 20:35:30 +0200

    `command` as helper and decorator

--- ember-command/src/helpers/command.ts
+++ ember-command/src/helpers/command.ts
@@ -1,20 +1,75 @@
-import Helper from '@ember/component/helper';
+// import Helper from '@ember/component/helper';
+import { capabilities, setHelperManager } from '@ember/helper';
 
-import { getOwner } from '../-private/-owner';
+import { decorate } from '../-private/decorator';
+// import { getOwner } from '../-private/-owner';
 import { createCommandInstance } from '../-private/instance';
 
 import type { Commandable, CommandInstance } from '../-private/instance';
 import type Owner from '@ember/owner';
+import type { HelperCapabilities } from '@glimmer/interfaces';
 
-export interface CommandHelperSignature {
-  Args: {
-    Positional: Commandable[];
-  };
-  Return: CommandInstance;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type NonInstanceType<K> = K extends InstanceType<any> ? object : K;
+type DecoratorKey<K> = K extends string | symbol ? K : never;
+
+interface Args {
+  positional: unknown;
+  named: object;
 }
 
-export default class CommandHelper extends Helper<CommandHelperSignature> {
-  compute(commands: Commandable[]): CommandInstance {
-    return createCommandInstance(getOwner(this) as Owner, commands);
+type CommandHelperSignature = (commandables: Commandable[], owner: Owner) => CommandInstance;
+
+class CommandHelperManager {
+  capabilities: HelperCapabilities = capabilities('3.23', {
+    hasValue: true
+  });
+
+  constructor(private owner: Owner | undefined) {}
+
+  createHelper(fn: CommandHelperSignature, args: Args) {
+    return { fn, args };
+  }
+
+  getValue({ fn, args }: { fn: CommandHelperSignature; args: Args }) {
+    return fn(args.positional as Commandable[], this.owner as Owner);
   }
 }
+
+interface DecoratorPropertyDescriptor extends PropertyDescriptor {
+  initializer?(): unknown;
+}
+
+/**
+ * The `@use` decorator can be used to use a Resource in javascript classes
+ *
+ * ```js
+ * import { command } from 'ember-resources';
+ *
+ * class MyC {
+ *   @command data = Clock;
+ * }
+ *
+ * ```
+ */
+export default function command<Prototype, Key>(
+  prototype: NonInstanceType<Prototype>,
+  key: DecoratorKey<Key>,
+  descriptor?: DecoratorPropertyDescriptor
+): void;
+
+export default function command(commandables: Commandable[]): CommandInstance;
+
+export default function command(
+  commandables: Commandable[] | object,
+  key?: string | symbol | Owner,
+  desc?: DecoratorPropertyDescriptor
+): void | CommandInstance {
+  if (key && desc && (desc.get || desc.initializer)) {
+    return decorate(undefined, key as string | symbol, desc) as unknown as void;
+  }
+
+  return createCommandInstance(key as Owner, commandables as Commandable[]);
+}
+
+setHelperManager((owner) => new CommandHelperManager(owner as Owner), command);
--- ember-command/src/index.ts
+++ ember-command/src/index.ts
@@ -1,10 +1,11 @@
 import { Command } from './-private/command';
-import { decorator } from './-private/decorator';
+// import { decorator } from './-private/decorator';
 import { commandFor } from './-private/instance';
 import { LinkCommand } from './-private/link-command';
+import command from './helpers/command';
 
 import type { Commandable, CommandAction, CommandInstance } from './-private/instance';
 
-export { Command, decorator as command, commandFor, LinkCommand };
+export { Command, command, commandFor, LinkCommand };
 
 export type { Commandable, CommandAction, CommandInstance };
