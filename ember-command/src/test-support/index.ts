907dc6244c75a92f9fdf5bb7cee055a4b0db48e0
gossi <gossi@users.noreply.github.com>
2023-10-08 15:43:08 +0200

    Remove deprecated `prepare*()` test support

--- ember-command/src/test-support/index.ts
+++ ember-command/src/test-support/index.ts
@@ -5,7 +5,6 @@ import { isTestContext } from '@ember/test-helpers/setup-context';
 import { setOwner } from '../-private/-owner';
 import { createCommandInstance } from '../-private/instance';
 
-import type { Command, CommandAction } from '../';
 import type { Commandable, CommandInstance } from '../-private/instance';
 import type { TestContext } from '@ember/test-helpers';
 
@@ -32,7 +31,7 @@ function getContext(): TestContext {
  * ```
  *
  * @param commandable The commandable(s)
- * @returns the prepare command action
+ * @returns the command instance
  */
 export function arrangeCommandInstance(commandable: Commandable | Commandable[]): CommandInstance {
   const context = getContext();
@@ -53,57 +52,4 @@ export function arrangeCommand<C extends Commandable>(command: C): C {
   setOwner(command, context.owner);
 
   return command;
-}
-
-/**
- * Use this to prepare a command for a rendering test putting that command into
- * a component that accepts one.
- *
- * @example
- *
- * ```ts
- * this.command = setupCommandAction(this, new FooBarCommand());
- * await render(hbs`<CommandElement @command={{this.command}}/>`);
- * ```
- *
- * @param context the test context
- * @param commandable The commandable(s)
- * @returns the prepare command action
- * @deprecated use `arrangeCommandAction()` instead
- */
-export function prepareCommandAction(
-  context: TestContext,
-  commandable: Commandable | Commandable[]
-): CommandAction {
-  return createCommandInstance(context.owner, commandable);
-}
-
-/**
- * Use this to prepare a command for integration testing by assigning the
- * owner to the command.
- *
- * @param context the test context
- * @param command The command
- * @returns The command with an assigned owner
- * @deprecated use `arrangeCommand()` instead
- */
-export function prepareCommand(context: TestContext, command: Command): Command {
-  setOwner(command, context.owner);
-
-  return command;
-}
-
-/**
- * Use this to prepare a command for integration testing by assigning the
- * owner to the command.
- *
- * @param context the test context
- * @param commandable Any commandable
- * @returns The commandable with an assigned owner
- * @deprecated use `arrangeCommand()` instead
- */
-export function prepareCommandable(context: TestContext, commandable: Commandable): Commandable {
-  setOwner(commandable, context.owner);
-
-  return commandable;
 }
