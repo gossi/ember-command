142af5467b7e0a8be77fcd2750a125601146fb0f
gossi <gossi@users.noreply.github.com>
2024-10-29 17:24:10 +0100

    With `@babel/plugin-transform-class-static-block`?

--- ember-command/babel.config.mjs
+++ ember-command/babel.config.mjs
@@ -4,6 +4,7 @@ export default {
     'ember-template-imports/src/babel-plugin',
     '@embroider/addon-dev/template-colocation-plugin',
     ['@babel/plugin-proposal-decorators', { legacy: true }],
+    '@babel/plugin-transform-class-static-block',
     '@babel/plugin-proposal-class-properties'
     // '@babel/plugin-transform-runtime'
   ]
