77395614fda3b20223b5940acb50689ba2eda2ab
gossi <gossi@users.noreply.github.com>
2025-06-28 10:33:35 +0200

    remove old code

--- test-app/app/actions/mutate-action.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from 'ember-command';
-
-export default class MutateAction extends Command {
-  execute(obj: Record<string, unknown>, change: Record<string, unknown>): void {
-    for (const [key, value] of Object.entries(change)) {
-      obj[key] = value;
-    }
-  }
-}
--- test-app/app/components/command-demo/push-log-command.ts
+++
@@ -1,8 +0,0 @@
-import { Command } from 'ember-command';
-
-export default class PushLogCommand extends Command {
-  execute(): void {
-    // eslint-disable-next-line no-console
-    console.log('puuuushed');
-  }
-}
