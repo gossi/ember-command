c0a0ad65027846dbae769e06283f8a7788660f28
gossi <gossi@users.noreply.github.com>
2025-06-30 13:52:27 +0200

    fix lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,7 +2,12 @@ import { configs } from '@gossi/config-eslint';
 
 export default [
   {
-    ignores: ['docs/.vitepress/cache/', 'docs/.vitepress/dist/']
+    ignores: [
+      'docs/.vitepress/cache/',
+      'docs/.vitepress/dist/',
+      // https://github.com/gossi/ember-command/issues/139
+      'tests/rendering/command-element-test.gts'
+    ]
   },
   ...configs.ember(import.meta.dirname),
   {
